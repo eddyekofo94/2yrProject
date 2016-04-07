071baac0d475cf6afe4ef0f63869c6fb331cffd5
phillip cheape <phillip.cheape@gmail.com>
2016-04-07 21:11:52 +0100

    register login now working

--- FootballManager/app/models/users/Login.java
+++ FootballManager/app/models/users/Login.java
@@ -5,7 +5,7 @@ public class Login{
     public String password;
     
     public String validate(){
-        if(SuperUser.authenticate(userid, password) == null){
+        if(User.authenticate(userid, password) == null){
             return "Invalid user or password";
         }
         else{
