c46d91d32746753c8a79d6087d6f5d403e93ebff
phillip cheape <phillip.cheape@gmail.com>
2016-04-10 15:59:54 +0100

    admin creation now working

--- FootballManager/app/controllers/AdminCtrl.java
+++ FootballManager/app/controllers/AdminCtrl.java
@@ -31,4 +31,51 @@ public class AdminCtrl extends Controller
 		
 		return ok(admin.render(User.getLoggedIn(session().get("loginname"))));
 	}
+	
+	
+	public Result addUserAdmin()
+	{
+		Form<User> registerForm = Form.form(User.class);
+		
+		return ok(addUserAdmin.render(User.getLoggedIn(session().get("loginname")),registerForm));
+	}
+	
+	
+	
+	
+	    public Result registerFormSubmit() {
+
+        //String saveImageMsg;
+
+        // Create a user form object (to hold submitted data)
+        // 'Bind' the object to the submitted form (this copies the filled form)
+        
+        Form<User> newRegisterForm = Form.form(User.class).bindFromRequest();
+
+        // Check for errors (based on Product class annotations)
+        //if (newRegisterForm.hasErrors()) {
+            // Display the form again
+            //return ok(register.render(User.getLoggedIn(session().get("userID")),newRegisterForm));
+        //}
+
+
+
+         CalcSHA cs = new CalcSHA();
+
+         User admin = newRegisterForm.get();
+         String md = cs.calcPassword(admin.password);
+
+        admin.password = md;
+         // Team team = new Team(k,"2",3);
+         // team =  user.RegisterUser(team);
+         // user.save();
+         // user.en(user.password);
+		 Administrator a1 = new Administrator(admin);
+         a1.save();
+
+        flash("success", "Admin " + newRegisterForm.get().name + " has been registered");
+
+        return redirect("/admin");
+    }
+	
 }
