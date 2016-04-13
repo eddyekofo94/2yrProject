b54b8167769781b681c2bb2a7e5c125da145d3af
yyyura <yyyura@gmail.com>
2016-04-13 11:30:14 +0100

    addPlayer.csala cleaned, flash added

--- FootballManager/app/models/users/Login.java
+++ FootballManager/app/models/users/Login.java
@@ -1,20 +1,20 @@
 package models.users;
+
 import models.*;
 
-public class Login{
+public class Login {
     public String loginname;
     public String password;
-    
-    public String validate(){
-		
-		CalcSHA cs = new CalcSHA();
-						String md = cs.calcPassword(this.password);
-						this.password = md;
-		 
-        if(User.authenticate(loginname, password) == null){
+
+    public String validate() {
+
+        CalcSHA cs = new CalcSHA();
+        String md = cs.calcPassword(this.password);
+        this.password = md;
+
+        if (User.authenticate(loginname, password) == null) {
             return "Invalid user or password";
-        }
-        else{
+        } else {
             return null;
         }
     }
